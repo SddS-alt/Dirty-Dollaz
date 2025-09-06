1603168bd7a2e3dbbef9b648f3fd33404e0dcb7e
SddS-alt <negrayona911@gmail.com>
2025-09-06 16:24:15 -0600

    Create page.js

---
+++ app/app/contact/page.js
@@ -0,0 +1,12 @@
+app/contact/page.js
+export default function Contact() {
+  return (
+    <main style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Contact Us</h1>
+      <p style={{ marginTop: "1rem", fontSize: "1.2rem" }}>
+        Got questions or want to collab?  
+        Reach us at: <strong>support@dirtydollaz.com</strong>
+      </p>
+    </main>
+  );
+}
